37fa5d12972781143cc5db749fc96372ce5cc8e2
Blake Haswell <haswell00@gmail.com>
2014-08-08 14:36:08 +1000

    Added code to parse parts. It can generate answers sometimes, but not always e.g. parse "3*5+2" results in Nothing.

--- Equation.hs
+++ Equation.hs
@@ -2,9 +2,12 @@ module Equation where
 --module Equation (parse, solve) where
 
 import Control.Applicative
+import Control.Monad
+import Data.Char (digitToInt, isDigit)
+import Debug.Trace
 
-data Equation = Empty | Leaf Double | Tree Operator Equation Equation
-data Operator = Plus | Minus | Multiply | Divide
+data Equation = Empty | Leaf Double | Tree Operator Equation Equation deriving (Show)
+data Operator = Plus | Minus | Multiply | Divide deriving (Show)
 type Parts = [String]
 
 -- 3 + 5 * 2
@@ -27,7 +30,7 @@ sampleEq' =
         )
         (Leaf 2)
 
-parse :: String -> Equation
+parse :: String -> Maybe Equation
 parse = parseParts . parseString
 
 parseString :: String -> Parts
@@ -39,8 +42,45 @@ parseString = filter (not . null) . foldr parse' []
                                 then (c:head xs):tail xs
                                 else [[c]]
 
-parseParts :: Parts -> Equation
-parseParts = undefined
+parseParts :: Parts -> Maybe Equation
+parseParts = foldr parse' (Just Empty)
+    where parse' p e = join $ joinEqs <$> parsePart p <*> e
+
+parsePart :: String -> Maybe Equation
+parsePart = foldr parse' (Just Empty)
+    where parse' c e = join $ joinEqsWithPrecendence <$> parseChar c <*> e
+
+parseChar :: Char -> Maybe Equation
+parseChar c
+    | isDigit c    = Just (Leaf ((fromIntegral . digitToInt) c))
+    | isOperator c = Just (Tree (operatorFromChar c) Empty Empty)
+    | otherwise    = Nothing
+        where isOperator = (`elem` "+-*/")
+              operatorFromChar '+' =  Plus
+              operatorFromChar '-' =  Minus
+              operatorFromChar '*' =  Multiply
+              operatorFromChar '/' =  Divide
+
+joinEqs :: Equation -> Equation -> Maybe Equation
+joinEqs Empty e1             = Just e1
+joinEqs e1 Empty             = Just e1
+joinEqs (Tree o e1 Empty) e2 = Just (Tree o e1 e2)
+joinEqs (Tree o Empty e1) e2 = Just (Tree o e1 e2)
+joinEqs e1 (Tree o Empty e2) = Just (Tree o e1 e2)
+joinEqs e1 (Tree o e2 Empty) = Just (Tree o e1 e2)
+joinEqs (Leaf a) (Leaf b)    = Just (Leaf ((a * 10) + b))
+joinEqs e1 e2                = traceShow (e1, e2) $ Nothing
+
+joinEqsWithPrecendence :: Equation -> Equation -> Maybe Equation
+joinEqsWithPrecendence (Tree Multiply e1 Empty) (Tree o e2 e3) = Just (Tree o (Tree Multiply e1 e2) e3)
+joinEqsWithPrecendence (Tree Multiply Empty e1) (Tree o e2 e3) = Just (Tree o (Tree Multiply e1 e2) e3)
+joinEqsWithPrecendence (Tree o e1 e2) (Tree Multiply Empty e3) = Just (Tree o (Tree Multiply e2 e3) e1)
+joinEqsWithPrecendence (Tree o e1 e2) (Tree Multiply e3 Empty) = Just (Tree o (Tree Multiply e2 e3) e1)
+joinEqsWithPrecendence (Tree Divide e1 Empty) (Tree o e2 e3) = Just (Tree o (Tree Divide e1 e2) e3)
+joinEqsWithPrecendence (Tree Divide Empty e1) (Tree o e2 e3) = Just (Tree o (Tree Divide e1 e2) e3)
+joinEqsWithPrecendence (Tree o e1 e2) (Tree Divide Empty e3) = Just (Tree o (Tree Divide e2 e3) e1)
+joinEqsWithPrecendence (Tree o e1 e2) (Tree Divide e3 Empty) = Just (Tree o (Tree Divide e2 e3) e1)
+joinEqsWithPrecendence e1 e2 = joinEqs e1 e2
 
 solve :: Equation -> Maybe Double
 solve Empty          = Nothing
