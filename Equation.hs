d16e247c2348ac42de9e8953d2e383f313507366
Blake Haswell <haswell00@gmail.com>
2014-08-12 22:22:46 +1000

    Using parsec's buildExpressionParser function to create a parser for equations.

--- Equation.hs
+++ Equation.hs
@@ -1,20 +1,15 @@
 module Equation where
 --module Equation (parse, solve) where
 
-import Control.Applicative hiding (many)
-import Control.Monad
-import Data.Char (digitToInt, isDigit)
-import Debug.Trace
-
-import Text.Parsec.Char (anyChar, char, digit)
+import Control.Applicative hiding (many, (<|>))
+import Text.Parsec.Char (char, digit)
 import Text.Parsec.Combinator (many1, option)
-import Text.Parsec.Expr (Assoc (..), Operator (..))
-import Text.Parsec.Prim (Parsec, many)
+import Text.Parsec.Expr (Assoc (..), Operator (..), buildExpressionParser)
+import qualified Text.Parsec.Prim as P ((<|>), parse)
 import Text.Parsec.String (Parser)
 
 data Equation = Empty | Leaf Double | Tree Op Equation Equation deriving (Show)
 data Op = Plus | Minus | Multiply | Divide deriving (Show)
-type Parts = [String]
 
 -- 3 + 5 * 2
 sampleEq :: Equation
@@ -37,75 +32,22 @@ sampleEq' =
         (Leaf 2)
 
 parse :: String -> Maybe Equation
-parse = parseParts . parseString
+parse s = either (const Nothing) Just (P.parse parseEquation "" s)
 
-parseString :: String -> Parts
-parseString = filter (not . null) . foldr parse' []
-    where
-        parse' c xs
-            | c `elem` "()" = []:xs
-            | otherwise     = if (not . null) xs
-                                then (c:head xs):tail xs
-                                else [[c]]
+parseEquation :: Parser Equation
+parseEquation = buildExpressionParser table parseSubExpression
+    where table     = [[ getOp '*' Multiply, getOp '/' Divide ],
+                       [ getOp '+' Plus,     getOp '-' Minus ]]
+          getOp c o = Infix (char c >> return (Tree o)) AssocLeft
 
-parseParts :: Parts -> Maybe Equation
-parseParts = foldr parse' (Just Empty)
-    where parse' p e = join $ joinEqs <$> parsePart p <*> e
-
-parsePart :: String -> Maybe Equation
-parsePart = foldr parse' (Just Empty)
-    where parse' c e = join $ joinEqsWithPrecendence <$> parseChar c <*> e
-
-parseChar :: Char -> Maybe Equation
-parseChar c
-    | isDigit c    = Just (Leaf ((fromIntegral . digitToInt) c))
-    | isOperator c = Just (Tree (operatorFromChar c) Empty Empty)
-    | otherwise    = Nothing
-        where isOperator = (`elem` "+-*/")
-              operatorFromChar '+' =  Plus
-              operatorFromChar '-' =  Minus
-              operatorFromChar '*' =  Multiply
-              operatorFromChar '/' =  Divide
-
-joinEqs :: Equation -> Equation -> Maybe Equation
-joinEqs Empty e1             = Just e1
-joinEqs e1 Empty             = Just e1
-joinEqs (Tree o e1 Empty) e2 = Just (Tree o e1 e2)
-joinEqs (Tree o Empty e1) e2 = Just (Tree o e1 e2)
-joinEqs e1 (Tree o Empty e2) = Just (Tree o e1 e2)
-joinEqs e1 (Tree o e2 Empty) = Just (Tree o e1 e2)
-joinEqs (Leaf a) (Leaf b)    = Just (Leaf ((a * 10) + b))
-joinEqs e1 e2                = traceShow (e1, e2) $ Nothing
-
-joinEqsWithPrecendence :: Equation -> Equation -> Maybe Equation
-joinEqsWithPrecendence (Tree Multiply e1 Empty) (Tree o e2 e3) = Just (Tree o (Tree Multiply e1 e2) e3)
-joinEqsWithPrecendence (Tree Multiply Empty e1) (Tree o e2 e3) = Just (Tree o (Tree Multiply e1 e2) e3)
-joinEqsWithPrecendence (Tree o e1 e2) (Tree Multiply Empty e3) = Just (Tree o (Tree Multiply e2 e3) e1)
-joinEqsWithPrecendence (Tree o e1 e2) (Tree Multiply e3 Empty) = Just (Tree o (Tree Multiply e2 e3) e1)
-joinEqsWithPrecendence (Tree Divide e1 Empty) (Tree o e2 e3) = Just (Tree o (Tree Divide e1 e2) e3)
-joinEqsWithPrecendence (Tree Divide Empty e1) (Tree o e2 e3) = Just (Tree o (Tree Divide e1 e2) e3)
-joinEqsWithPrecendence (Tree o e1 e2) (Tree Divide Empty e3) = Just (Tree o (Tree Divide e2 e3) e1)
-joinEqsWithPrecendence (Tree o e1 e2) (Tree Divide e3 Empty) = Just (Tree o (Tree Divide e2 e3) e1)
-joinEqsWithPrecendence e1 e2 = joinEqs e1 e2
-
---parsecParse :: Parser Equation
---parsecParse = do
---    d <- many digit
---    return $ Leaf (read d)
+parseSubExpression :: Parser Equation
+parseSubExpression = char '(' *> parseEquation <* char ')'
+               P.<|> parseDouble
 
 parseDouble :: Parser Equation
---parseDouble = do
---    i <- many1 digit
---    f <- option "" $ (:) <$> (char '.') <*> many1 digit
---    --f <- many digit
---    return $ Leaf (read (i ++ f))
-parseDouble =
-    Leaf <$> (read <$> ((++) <$> integer <*> fraction))
-        where integer = many1 digit
-              fraction = option "" $ (:) <$> (char '.') <*> many1 digit
-
---multiply = char '*'
---multiply = Infix ((char '*') >> return (Tree Multiply)) AssocLeft
+parseDouble = fmap (Leaf . read) $ (++) <$> integer <*> fraction
+    where integer  = many1 digit
+          fraction = option "" $ (:) <$> char '.' <*> many1 digit
 
 solve :: Equation -> Maybe Double
 solve Empty          = Nothing
